64954f62fa263a06bdbe83fe255c6979019e198b
jjia-tn <jjia.tn@gmail.com>
2023-01-20 14:22:47 +0800

    complete game

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -17,6 +19,7 @@ public final class App {
         String[] tttBoard = new String[9];
         // player X and O
         String player = "X";
+        String winner = "";
 
         // populating the tic-tac-toe board with 1 to 9
         for (int i = 0; i < 9; i++) {
@@ -28,8 +31,52 @@ public final class App {
         // print out the initial tic-tac-toe board
         ttt.printBoard(tttBoard);
 
-        System.out.println("Tic-Tac-Toe game");
-        System.out.println("-----------------");
-        System.out.println("X will play first");
+        Scanner scanner = new Scanner(System.in);
+
+        while (winner == "") {
+
+            System.out.println("Tic-Tac-Toe game");
+            System.out.println("-----------------");
+            System.out.println(player + " will play now...");
+            
+            // play tic-tac-toe logic here...
+            Integer input;
+
+            System.out.println("(Player " + player + ") Enter your position: ");
+            input = scanner.nextInt();
+
+            // only accept keyboard input 1 to 9
+            if ((input > 0) && (input < 10)) {
+
+            } else {
+                System.out.println("Invalid input: Only number 1 to 9 is allowed.");
+                continue;
+            }
+
+            // assign 'X' or 'O' to position if it is number
+            if ((tttBoard[input - 1]).equals(String.valueOf(input))) {
+                tttBoard[input - 1] = player;
+
+                // switch to another player's turn
+                if (player.equalsIgnoreCase("X")) {
+                    player = "O";
+                } else {
+                    player = "X";
+                }
+            } else {
+                System.out.println("Position already taken up. Please enter another position number");
+            }
+
+            ttt.printBoard(tttBoard);
+            winner = ttt.checkWinner(tttBoard);
+        }
+            
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println("It's a " + winner);
+        } else {
+            System.out.println("The winner of this game is " + winner);
+        }
+
+        scanner.close();
     }
 }
